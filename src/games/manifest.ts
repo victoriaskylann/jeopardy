60a4267920ac400b07d7dc29a5f89c04ce9402cd
Victoria Black <victoria.skylan@gmail.com>
2026-05-28 11:29:09 -0600

    Add two more pre-bundled games (90s Pop Culture, Science & Nature)

--- src/games/manifest.ts
+++ src/games/manifest.ts
@@ -1,7 +1,13 @@
 import type { Game } from '../types';
 import generalTrivia from './general-trivia.json';
+import nineties from './90s-pop-culture.json';
+import science from './science-and-nature.json';
 
-export const GAMES: Game[] = [generalTrivia as Game];
+export const GAMES: Game[] = [
+  generalTrivia as Game,
+  nineties as Game,
+  science as Game,
+];
 
 export function getGameById(id: string): Game | undefined {
   return GAMES.find((g) => g.id === id);
